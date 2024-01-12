2c013842620693395a24a7a052abd45520004043
younglewy23 <89994128+younglewy23@users.noreply.github.com>
2024-01-11 20:37:02 -0600

    Update set_env.py

--- set_env.py
+++ set_env.py
@@ -5,3 +5,6 @@ import os
 os.environ['SPOTIPY_CLIENT_ID'] = '8080f77f678c4b3a8d457e2fffcee4c8'
 os.environ['SPOTIPY_CLIENT_SECRET'] = 'b114ec411871498b907d98d2c608ba93'
 
+import os
+os.environ['SPOTIPY_CLIENT_ID'] = '36f5dc37a32d4c0ca2e8d37c79fede46'
+os.environ['SPOTIPY_CLIENT_SECRET'] = '22a67d31b3a6478e9847d957f8101dc7'
